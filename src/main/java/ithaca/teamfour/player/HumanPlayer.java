332a9b5e5c5165d429d7d11572f964c3e7987ec3
Morgan <morgan.e.ziegler@gmail.com>
2021-04-15 00:01:46 -0400

    Fixed some bugs

--- src/main/java/ithaca/teamfour/player/HumanPlayer.java
+++ src/main/java/ithaca/teamfour/player/HumanPlayer.java
@@ -14,12 +14,45 @@ public class HumanPlayer implements ChessPlayer{
     private Scanner keyboard = new Scanner(System.in);
     
     public Move chooseMove(Board curBoard, char yourSymbol){
-        String locFrom = "from";
-        System.out.println("Location from: ");
-        locFrom = keyboard.nextLine();
-        String locTo = "to";
-        System.out.println("Location to: ");
-        locTo = keyboard.nextLine();
-        return new Move( Square.fromValue(locFrom.toUpperCase()),  Square.fromValue(locTo.toUpperCase()));
+        Move temp = null;
+        int good = 0;
+        while (good != 1){
+        System.out.print(yourSymbol + ", enter coorinates for your next move x,y:");
+            String coordString = keyboard.nextLine();
+            while (readAndVerifyCoords(coordString) == null){
+                System.out.print("bad coordinates, try again, format x,y:");
+                coordString = keyboard.nextLine();
+            }
+            temp = readAndVerifyCoords(coordString);
+
+            if (yourSymbol==('W')){
+                if (curBoard.getPiece(temp.getFrom()).getPieceSide().toString().contains("WHITE")){
+                    good = 1;
+                } 
+            }
+            else if(curBoard.getPiece(temp.getFrom()).getPieceSide().toString().contains("BLACK")){
+                good = 1;
+            }
+            else{
+                System.out.println("Please move your own piece");
+            }
+        }
+        return temp;
+    }
+
+    public static Move readAndVerifyCoords(String coordString){
+        String[] coords = coordString.split(",");
+        Square to;
+        Square from;
+        try{
+            to = Square.fromValue(coords[0].toUpperCase());
+            from= Square.fromValue(coords[1].toUpperCase());
+
+        }
+        catch(Exception e){
+            return null;
+        }
+
+        return (new Move(to,from));
     }
 }
