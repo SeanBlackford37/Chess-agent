bca83a4fe5da6123426a3cf6312486d1729502b3
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2021-04-16 12:04:58 -0400

    added SimpleReflexAgent

--- src/main/java/ithaca/teamfour/ChessMain.java
+++ src/main/java/ithaca/teamfour/ChessMain.java
@@ -5,6 +5,7 @@ import com.github.bhlangonijr.chesslib.Board;
 import ithaca.teamfour.player.ChessGame;
 import ithaca.teamfour.player.HumanPlayer;
 import ithaca.teamfour.player.RandomPlayer;
+import ithaca.teamfour.player.SimpleReflexAgentSean;
 
 public class ChessMain {
     public static void main(String[] args){
@@ -30,8 +31,8 @@ public class ChessMain {
     board.movePiece(new Move(Square.E4, Square.D5), new MoveBackup());
     System.out.println(board.toString());
     */
-
-    ChessGame game = new ChessGame(new Board(), new HumanPlayer(), new RandomPlayer());
+                                                //White                 //Black
+    ChessGame game = new ChessGame(new Board(), new RandomPlayer(), new SimpleReflexAgentSean());
     game.play();
 
     }
